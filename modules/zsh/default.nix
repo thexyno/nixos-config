6fc70aca93a56f98491b7532f85bad93fde1dcb7
Philipp Hochkamp <me@phochkamp.de>
2021-03-15 20:35:29 +0100

    epic

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -11,7 +11,7 @@ in
 
   interactiveShellInit =
     let
-      zshrc = builtins.fileContents ./zshrc;
+      zshrc = builtins.readFile ./zshrc;
 
       sources = [
         "${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/git/git.plugin.zsh"
