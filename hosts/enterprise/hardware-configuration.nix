e1530656aa139c41e84d1c1dc19b0cb3dcbb119c
System administrator <root@nixos.de>
2021-03-15 14:52:44 +0000

    hardware configuration

--- hosts/enterprise/hardware-configuration.nix
+++ hosts/enterprise/hardware-configuration.nix
@@ -5,8 +5,7 @@
 
 {
   imports =
-    [
-      (modulesPath + "/profiles/qemu-guest.nix")
+    [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
@@ -15,14 +14,31 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/219e5c68-1453-4c35-8838-d0aa07a34fd4";
-      fsType = "ext4";
+    { device = "none";
+      fsType = "tmpfs";
+      options = [ "size=3G", "defaults", "mode=755" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/88cad3d1-2e42-495c-988f-377fa4fcd3b6";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/88cad3d1-2e42-495c-988f-377fa4fcd3b6";
+      fsType = "btrfs";
+      options = [ "subvol=var/log" ];
+    };
+
+  fileSystems."/persistent" =
+    { device = "/dev/disk/by-uuid/88cad3d1-2e42-495c-988f-377fa4fcd3b6";
+      fsType = "btrfs";
+      options = [ "subvol=persistent" ];
     };
 
   fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/6BC9-754B";
+    { device = "/dev/disk/by-uuid/1259-3A7D";
       fsType = "vfat";
     };
 
