dec0056313b80b207db829b4bc2d235759f19537
Philipp Hochkamp <me@phochkamp.de>
2021-03-15 21:20:44 +0100

    Undoing last comming

--- data/pubkeys.nix
+++ data/pubkeys.nix
@@ -7,8 +7,10 @@ let
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJwOH1b6xWmEr1VZh48kBIYhW11vtPFR3my8stAHlSi"
     ];
   in
+   {
     inherit pc work;
     computers = pc ++ work;
+  };
 in {
   inherit ragon;
 }
