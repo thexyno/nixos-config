54a63f8a0acb3f0deebeb4fda35b72c849ddf084
Philipp Hochkamp <me@phochkamp.de>
2021-03-15 19:36:12 +0100

    vim refactor

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -6,37 +6,38 @@ in
 {
   options.ragon.nvim.enable = lib.mkEnableOption "Enables ragons nvim config";
   config = lib.mkIf cfg.enable {
-  environment.systemPackages = with pkgs; [
-    (neovim.override {
-      vimAlias = true;
-      configure = {
-      plug.plugins = with pkgs.vimPlugins // sources; [
-        nnn-vim
-        vista-vim
-        undotree
-        polyglot
-        rainbow
-        vim-commentary
-        vim-table-mode
-        vim-pandoc
-        vim-pandoc-syntax
-        vim-speeddating
-        vim-nix
-        gruvbox
-        ultisnips
-        incsearch-vim
-        vim-highlightedyank
-        vim-fugitive
-        lightline-vim
-        fzf-vim
-        vim-devicons
-        coc-nvim
-      ];
-};
-      customRC = (builtins.readFile ./nvim/init.vim);
-    })
-  ];
-    
+    environment.systemPackages = with pkgs; [
+      (neovim.override {
+        vimAlias = true;
+        viAlias = true;
+        customRC = (builtins.readFile ./nvim/init.vim);
+        configure = {
+        plug.plugins = with pkgs.vimPlugins // sources; [
+          nnn-vim
+          vista-vim
+          undotree
+          polyglot
+          rainbow
+          vim-commentary
+          vim-table-mode
+          vim-pandoc
+          vim-pandoc-syntax
+          vim-speeddating
+          vim-nix
+          gruvbox
+          ultisnips
+          incsearch-vim
+          vim-highlightedyank
+          vim-fugitive
+          lightline-vim
+          fzf-vim
+          vim-devicons
+          coc-nvim
+        ];
+        };
+      })
+    ];
+      
     environment.etc."nvim/coc-settings.json".text = (builtins.readFile ./nvim/coc-settings.json);
 
 
