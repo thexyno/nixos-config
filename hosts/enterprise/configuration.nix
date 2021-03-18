eb7cce3cd6e7e7a2770044f841c15a029f08d56b
Philipp Hochkamp <me@phochkamp.de>
2021-03-19 00:38:30 +0100

    a

--- hosts/enterprise/configuration.nix
+++ hosts/enterprise/configuration.nix
@@ -70,7 +70,7 @@ in
     options = let
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in ["${automount_opts},credentials=/etc/smb-secrets"];
+      in ["${automount_opts},credentials=/etc/smb-secrets,uid=1000,gid=1"];
 
   };
 
