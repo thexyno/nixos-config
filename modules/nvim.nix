7823c6628dfd30af15f7354e35bf09c40123b5eb
Philipp Hochkamp <me@phochkamp.de>
2021-03-18 13:39:26 +0100

    arogip

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -9,6 +9,7 @@ in
   config = lib.mkIf cfg.enable {
   environment.systemPackages = with pkgs; [
     nodejs
+    sources.rnix-lsp
     (neovim.override {
       vimAlias = true;
       viAlias = true;
