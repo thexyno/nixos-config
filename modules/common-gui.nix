e2cb2a2928bac14509e5f5f4ecd6248acf7122c0
Philipp Hochkamp <me@phochkamp.de>
2021-03-24 11:26:39 +0100

    cinnamon

--- modules/common-gui.nix
+++ modules/common-gui.nix
@@ -55,6 +55,8 @@ in
     # List packages installed in system profile. To search by name, run:
     # $ nix-env -qaP | grep wget
     environment.systemPackages = with pkgs; [
+      cinnamon.nemo
+      arc-icon-theme
       feh
       pulsemixer
       firefox
