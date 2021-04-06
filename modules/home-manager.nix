74917ca527762c148b8843e14d705865644c183d
Philipp Hochkamp <me@phochkamp.de>
2021-04-06 02:03:26 +0200

    zathura

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -162,6 +162,7 @@ in
           enable = isGui;
           defaultApplications = {
             "text/html" = [ "firefox.desktop" ];
+            "application/pdf" = [ "zathura.desktop" ];
             "x-scheme-handler/http" = [ "firefox.desktop" ];
             "x-scheme-handler/https" = [ "firefox.desktop" ];
             "x-scheme-handler/about" = [ "firefox.desktop" ];
