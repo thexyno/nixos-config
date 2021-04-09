3ceb23bffcf195a5c9eb60d0e384175adb94552e
Philipp Hochkamp <me@phochkamp.de>
2021-04-09 22:54:19 +0200

    gpntrüagrnio

--- modules/develop.nix
+++ modules/develop.nix
@@ -14,6 +14,7 @@ in
     ];
     environment.systemPackages = with pkgs; [
       direnv # needed for lorri
+      unixtools.xxd
       nixos-generators
       android-studio
       android-udev-rules
