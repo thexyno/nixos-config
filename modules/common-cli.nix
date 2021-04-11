86c09ffd776347530f8471f7768a6ea54b29ee1e
Philipp Hochkamp <me@phochkamp.de>
2021-04-11 19:51:24 +0200

    disable sudo lecture

--- modules/common-cli.nix
+++ modules/common-cli.nix
@@ -28,6 +28,8 @@ in
     services.openssh.enable = true;
     services.openssh.passwordAuthentication = false;
 
+    secruity.sudo.extraConfig = "Defaults lecture = never";
+
     # firewall
     # networking.firewall.enable = true;
     # networking.firewall.allowPing = true;
