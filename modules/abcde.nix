dac3a501d7c23935032c826046c2304457603b75
Philipp Hochkamp <me@phochkamp.de>
2021-04-12 21:18:14 +0200

    hey, what's that over there?!

--- modules/abcde.nix
+++ modules/abcde.nix
@@ -21,13 +21,13 @@ in
       OUTPUTDIR="/media/data/Musik"               
       # Decide here how you want the tracks labelled for a standard 'single-artist',
       # multi-track encode and also for a multi-track, 'various-artist' encode:
-      OUTPUTFORMAT='''${OUTPUT}/''${ARTISTFILE}/''${ALBUMFILE}/''${TRACKNUM}.''${TRACKFILE}'
-      VAOUTPUTFORMAT='''${OUTPUT}/Various/''${ALBUMFILE}/''${TRACKNUM}.''${ARTISTFILE}-''${TRACKFILE}'
+      OUTPUTFORMAT=' ''${OUTPUT}/''${ARTISTFILE}/''${ALBUMFILE}/''${TRACKNUM}.''${TRACKFILE}'
+      VAOUTPUTFORMAT=' ''${OUTPUT}/Various/''${ALBUMFILE}/''${TRACKNUM}.''${ARTISTFILE}-''${TRACKFILE}'
       # Decide here how you want the tracks labelled for a standard 'single-artist',
       # single-track encode and also for a single-track 'various-artist' encode.
       # (Create a single-track encode with 'abcde -1' from the commandline.)
-      ONETRACKOUTPUTFORMAT='''${OUTPUT}/''${ARTISTFILE}/''${ALBUMFILE}/''${ALBUMFILE}'
-      VAONETRACKOUTPUTFORMAT='''${OUTPUT}/Various/''${ALBUMFILE}/''${ALBUMFILE}'
+      ONETRACKOUTPUTFORMAT=' ''${OUTPUT}/''${ARTISTFILE}/''${ALBUMFILE}/''${ALBUMFILE}'
+      VAONETRACKOUTPUTFORMAT=' ''${OUTPUT}/Various/''${ALBUMFILE}/''${ALBUMFILE}'
       mungefilename ()
       {
         echo "$@" | sed -e 's/^\.*//' | tr -d ":><|*/\"'?[:cntrl:]"
