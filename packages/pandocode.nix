6edc83b1a9b7a3cb3f59a13e2da7162b39329f07
Philipp Hochkamp <me@phochkamp.de>
2021-04-27 05:39:19 +0200

    ragop

--- packages/pandocode.nix
+++ packages/pandocode.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, stdenv, python3, python3Packages, zip }:
+{ lib, stdenv, python3, python3Packages, zip }:
 let
   py = python3.withPackages (pythonPackages: with pythonPackages; [ panflute ]);
 in
@@ -6,7 +6,10 @@ stdenv.mkDerivation rec {
   version = "1.0.1";
   name = "pandocode-${version}";
   nativeBuildInputs = [ zip ];
-  src = inputs.pandocode;
+  src = fetchTarball {
+    url = "https://github.com/nzbr/pandocode/archive/8f021538b71029e7f9efa7d04b4dfffd4d72a0ca.tar.gz";
+    sha256 = "1wadrv5mfyhk4qvglc398wprisn4wg2v44ghamgb79zqr3znhi1q";
+  };
   format = "other";
   doCheck = false;
   buildPhase = ''
