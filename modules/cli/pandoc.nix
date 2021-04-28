146c5f1e20861e96bea23b97e067393e7b049dc8
Philipp Hochkamp <me@phochkamp.de>
2021-04-28 19:48:19 +0200

    rigaon

--- modules/cli/pandoc.nix
+++ modules/cli/pandoc.nix
@@ -8,7 +8,7 @@ in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       pandoc
-      packages.pandocode
+      my.pandocode
       pandoc-plantuml-filter
       haskellPackages.pandoc-include-code
       texlive.combined.scheme-full
