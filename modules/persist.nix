bef5bdaf4f2d7160179ed9d58266bed2d2444ee5
Philipp Hochkamp <me@phochkamp.de>
2021-04-28 02:10:32 +0200

    ajax-loader hotness, oh yeah

--- modules/persist.nix
+++ modules/persist.nix
@@ -15,6 +15,7 @@ in
       ];
       files = [
         "/etc/machine-id"
+        "/etc/shadow" # TODO MAKE THIS LESS HACKY
         "/etc/ssh/ssh_host_rsa_key"
         "/etc/ssh/ssh_host_rsa_key.pub"
         "/etc/ssh/ssh_host_ed25519_key"
