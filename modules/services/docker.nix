c71ab3b25d3b05ef50b613992375da9564aa0cfe
Philipp Hochkamp <me@phochkamp.de>
2021-04-30 06:10:31 +0200

    First Blood

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -3,7 +3,7 @@ let
   cfg = config.ragon.services.docker;
 in
 {
-  options.ragon.services.docker.enable = lib.mkEnableOption "Enables docker"
+  options.ragon.services.docker.enable = lib.mkEnableOption "Enables docker";
   config = mkIf cfg.enable {
     virtualisation.docker.enable = true;
     ragon.user.extraGroups = [ "docker" ];
