3c8e67b9ad9dda964e183ba6f0bbbb6099911357
Philipp Hochkamp <me@phochkamp.de>
2021-05-01 17:44:01 +0200

    initial voyager

---
+++ hosts/voyager/default.nix
@@ -0,0 +1,32 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  networking.useDHCP = false;
+  networking.networkmanager.enable = true;
+  networking.hostId = "7b45236b";
+
+  # Immutable users due to tmpfs
+  users.mutableUsers = false;
+
+
+  ragon.cli.enable = true;
+  ragon.cli.pandoc.enable = true;
+  ragon.user.enable = true;
+  ragon.home-manager.enable = true;
+  ragon.gui.enable = true;
+  ragon.develop.enable = true;
+  ragon.persist.enable = true;
+  ragon.services.docker.enable = true;
+  ragon.services.ssh.enable = true;
+
+}
