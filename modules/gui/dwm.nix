3ca6e0b20ed710c9c1d265238f8dcf280ee297f7
Philipp Hochkamp <me@phochkamp.de>
2021-05-03 02:04:03 +0200

    _

--- modules/gui/dwm.nix
+++ modules/gui/dwm.nix
@@ -208,7 +208,7 @@ in
             { battery_perc,    "BAT: %s | ",           "BAT0" },
             { run_command,    "LIGHT: %s | ",           "cat /sys/class/backlight/intel_backlight/brightness" },
           '' ;
-          nonlaptopargs = lib.mkIf cfg.laptop == false ''
+          nonlaptopargs = lib.mkIf !cfg.laptop ''
             { run_command,    "MOUSE: %s | ",           "cat /sys/class/power_supply/hidpp_battery_*/capacity_level | sed 's/Unknown/Charging/'" },
             { disk_free,   "NAS: %s | ",           "/media/data" },
           '';
