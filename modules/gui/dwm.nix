b6d11df57922fe3b2c7f90dddeb3b42b5d78881d
Philipp Hochkamp <me@phochkamp.de>
2021-05-03 15:29:28 +0200

    freemasonry

--- modules/gui/dwm.nix
+++ modules/gui/dwm.nix
@@ -208,7 +208,7 @@ in
             { battery_perc,    "BAT: %s | ",           "BAT0" },
             { run_command,    "LIGHT: %s | ",           "cat /sys/class/backlight/intel_backlight/brightness" },
           '' ;
-          nonlaptopargs = lib.optionalString !cfg.laptop ''
+          nonlaptopargs = lib.optionalString (cfg.laptop == false) ''
             { run_command,    "MOUSE: %s | ",           "cat /sys/class/power_supply/hidpp_battery_*/capacity_level | sed 's/Unknown/Charging/'" },
             { disk_free,   "NAS: %s | ",           "/media/data" },
           '';
