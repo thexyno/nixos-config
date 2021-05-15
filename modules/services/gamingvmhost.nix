2c916b174aa6363a20cb5ef612a08ad6de2e22c5
Philipp Hochkamp <me@phochkamp.de>
2021-05-15 22:59:06 +0200

    added ASMedia USB 3.1 to gamingvmhost

--- modules/services/gamingvmhost.nix
+++ modules/services/gamingvmhost.nix
@@ -250,6 +250,12 @@ in
                       </source>
                       <address type='pci' domain='0x0000' bus='0x05' slot='0x00' function='0x0'/>
                     </hostdev>
+                    <hostdev mode="subsystem" type="pci" managed="yes">
+                      <source>
+                        <address domain="0x0000" bus="0x0b" slot="0x00" function="0x0"/>
+                      </source>
+                      <address type="pci" domain="0x0000" bus="0x01" slot="0x00" function="0x0"/>
+                    </hostdev>
                     <redirdev bus='usb' type='spicevmc'>
                       <address type='usb' bus='0' port='1'/>
                     </redirdev>
