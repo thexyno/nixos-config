21db2da2d0de571437345afdf01078933dce6fd7
Philipp Hochkamp <me@phochkamp.de>
2021-05-25 01:51:48 +0200

    asdf

---
+++ modules/services/signal.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.ragon.services.signal;
+in
+{
+  options.ragon.services.signal.enable = lib.mkEnableOption "Enables signal-cli-rest-api";
+  config = lib.mkIf cfg.enable {
+    virtualisation.oci-containers.backend = "podman";
+    virtualisation.oci-contaiers.containers."signal-cli-rest-api" = {
+      image = "bbernhard/signal-cli-rest-api";
+      volumes = [
+        "/var/lib/signal:/home/.local/share/signal-cli"
+      ];
+    };
+    persist.extraDirectories = [
+      "/var/lib/signal"
+    ];
+  };
+}
