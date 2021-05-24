c9dbc1e1103d9b84d1a56ab370b716c25b90d071
Philipp Hochkamp <me@phochkamp.de>
2021-05-25 01:47:23 +0200

    aaaaaaaa

---
+++ modules/services/home-assistant/lovelace.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.ragon.services.home-assistant;
+in
+{
+  config = lib.mkIf cfg.enable {
+    # https://github.com/Mic92/dotfiles/tree/master/nixos/eve/modules/home-assistant for orientation
+    services.home-assistant.lovelaceConfig = {
+      title = "The mighty Citadel of Dundee";
+      views = [
+        { # dashboard
+          title = "The Chaos Portal to the galactic nexus";
+          cards = [
+
+          ];
+
+        }
+      ];
+
+    };
+}
