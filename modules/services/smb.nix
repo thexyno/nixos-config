9c9e10d742552fc78747a924d80dd8c53aa8aa31
Philipp Hochkamp <me@phochkamp.de>
2021-05-25 02:05:33 +0200

    Fix my stupidness

--- modules/services/smb.nix
+++ modules/services/smb.nix
@@ -5,8 +5,6 @@ in
 {
   options.ragon.services.smb.enable = lib.mkEnableOption "Enables samba";
   config = lib.mkIf cfg.enable {
-    services.
-
     networking.firewall.allowedTCPPorts = [ 445 139 ];
     networking.firewall.allowedUDPPorts = [ 137 138 ];
   };
