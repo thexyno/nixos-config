7f892a549ad334aca456796b56db2b47f29718a6
Philipp Hochkamp <me@phochkamp.de>
2021-05-30 17:31:18 +0200

    Never gonna give you up

--- lib/nixos.nix
+++ lib/nixos.nix
@@ -1,17 +1,17 @@
-{ inputs, lib, pkgs, self, ... }:
+{ inputs, lib, pkgs, ... }:
 
 with lib;
 with lib.my;
 let sys = "x86_64-linux";
 in {
-  mkHost = path: attrs @ { system ? sys, ... }:
+  mkHost = path: attrs @ { system ? sys, overlay, overlays, ... }:
     nixosSystem {
       inherit system;
       specialArgs = { inherit lib inputs system; };
       modules = [
         {
           nixpkgs.pkgs = pkgs;
-          nixpkgs.overlays = [ self.overlay ];
+          nixpkgs.overlays = [ overlay ] ++ overlays;
           networking.hostName = mkDefault (removeSuffix ".nix" (baseNameOf path));
         }
         (filterAttrs (n: v: !elem n [ "system" ]) attrs)
