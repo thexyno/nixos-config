6b0a8645cac21e866cb796d3ae3ac6a9a73725e1
Philipp Hochkamp <me@phochkamp.de>
2021-06-01 01:23:29 +0200

    I honestly wish I could remember what was going on here...

--- packages/pridecat.nix
+++ packages/pridecat.nix
@@ -1,7 +1,10 @@
 { lib, fetchFromGitHub, stdenv }:
+let
+  pname = "pridecat";
+in
 stdenv.mkDerivation rec {
   version = "1.0.0";
-  name = "pridecat-${version}";
+  pname = pname;
   src = fetchFromGitHub {
     owner = "lunasorcery";
     repo = "pridecat";
@@ -9,6 +12,10 @@ stdenv.mkDerivation rec {
     sha256 = "PyGLbbsh9lFXhzB1Xn8VQ9zilivycGFEIc7i8KXOxj8=";
     fetchSubmodules = true;
   };
+
+  installPhase = ''
+    install -D -m 755 ${pname} $out/bin/${pname}
+  '';
 
   meta = with lib; {
     description = "Like cat, but more colorful - inspired by lolcat.";
