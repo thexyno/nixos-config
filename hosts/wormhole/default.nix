ac1e822b5e8e182a1f47efbb21aa1ecd63f780b9
Philipp Hochkamp <me@phochkamp.de>
2021-06-05 19:44:39 +0200

    added wormhole secret

--- hosts/wormhole/default.nix
+++ hosts/wormhole/default.nix
@@ -1,27 +1,16 @@
 { config, inputs, pkgs, lib, ... }:
 {
   imports = [
-    "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
+    "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
   ];
   boot = {
     kernelPackages = pkgs.linuxPackages_5_11;
-    tmpOnTmpfs = true;
-    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
-    # ttyAMA0 is the serial console broken out to the GPIO
-    kernelParams = [
-      "8250.nr_uarts=1"
-      "console=ttyAMA0,115200"
-      "console=tty1"
-      # Some gui programs need this
-      "cma=128M"
-    ];
   };
   boot.loader.raspberryPi = {
     enable = true;
     version = 4;
   };
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
+
   nix = {
     autoOptimiseStore = true;
     gc = {
