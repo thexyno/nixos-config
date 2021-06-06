3730b225a7cbd7ca11e5c06db773cdb7d6232dd5
Philipp Hochkamp <me@phochkamp.de>
2021-06-06 03:28:23 +0200

    Add Sandbox

--- modules/services/home-assistant/default.nix
+++ modules/services/home-assistant/default.nix
@@ -19,6 +19,7 @@ in
         extraPackages = ps: with ps; [
           psycopg2
           colorlog
+          spotipy
         ];
       }).overrideAttrs (oldAttrs: {
         doInstallCheck = false;
