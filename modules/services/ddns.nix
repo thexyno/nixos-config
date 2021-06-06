fead3b8c2f9550e360cde8c929b5f299fa64671b
Philipp Hochkamp <me@phochkamp.de>
2021-06-06 02:10:59 +0200

    abcdefge

--- modules/services/ddns.nix
+++ modules/services/ddns.nix
@@ -4,10 +4,10 @@ let
   domain = config.ragon.services.nginx.domain;
   dataDir = "/var/lib/inadyn";
   cacheDir = "/var/cache/inadyn";
-  checkipv6 = pkgs.writeScript "checkipv6.sh" ''
-    #!${pkgs.bash}/bin/bash
-    ${pkgs.iproute2}/bin/ip address show scope global | grep inet6 | ${pkgs.gawk}/bin/awk '{print substr($$2, 1, length($$2)-3)}' | head -n 1
-  '';
+#  checkipv6 = pkgs.writeScript "checkipv6.sh" ''
+#    #!${pkgs.bash}/bin/bash
+#    ${pkgs.iproute2}/bin/ip address show scope global | grep inet6 | ${pkgs.gawk}/bin/awk '{print substr($$2, 1, length($$2)-3)}' | head -n 1
+#  '';
 in
 {
   options.ragon.services.ddns.enable = lib.mkEnableOption "Enables CloudFlare DDNS to the domain specified in ragon.services.nginx.domain and all subdomains";
@@ -25,16 +25,21 @@ in
           cat >/run/${RuntimeDirectory}/inadyn.cfg <<EOF
           period = 180
           user-agent = Mozilla/5.0
-          provider cloudflare.com {
-            username = ${domain}
-            password = $CLOUDFLARE_DNS_API_TOKEN
-            hostname = ${domain}
-          }
+          allow-ipv6 = true
+
+          # ipv4
           provider cloudflare.com:1 {
             username = ${domain}
             password = $CLOUDFLARE_DNS_API_TOKEN
             hostname = ${domain}
-            checkip-command = "${checkipv6}"
+          }
+          # ipv6
+          provider cloudflare.com:2 {
+            checkip-server = dns64.cloudflare-dns.com
+            checkip-path = /cdn-cgi/trace
+            username = ${domain}
+            password = $CLOUDFLARE_DNS_API_TOKEN
+            hostname = ${domain}
           }
           EOF
           exec ${pkgs.inadyn}/bin/inadyn -n --cache-dir=${cacheDir} -f /run/${RuntimeDirectory}/inadyn.cfg
