de9259bdc12f27c56c8e0e573c678fee1e12ff74
Philipp Hochkamp <me@phochkamp.de>
2021-06-16 03:14:05 +0200

    make htop shut up

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -79,7 +79,7 @@ in
           autorandr.profiles = {
             "work" = {
               fingerprint = {
-                DP-2-2  = "00ffffffffffff0005e30124b24407001d1e010380341d782a9b15a655519d260d5054bfef00d1c0b30095008180814081c001010101023a801871382d40582c450009252100001e000000ff00474d584c374841343736333338000000fc0032344231570a20202020202020000000fd00324c1e5311000a202020202020011802031ef14b101f051404130312021101230907078301000065030c0010008c0ad08a20e02d10103e9600092521000018011d007251d01e206e28550009252100001e8c0ad08a20e02d10103e96000925210000188c0ad090204031200c40550009252100001800000000000000000000000000000000000000000000000000f1";
+                DP-2-2 = "00ffffffffffff0005e30124b24407001d1e010380341d782a9b15a655519d260d5054bfef00d1c0b30095008180814081c001010101023a801871382d40582c450009252100001e000000ff00474d584c374841343736333338000000fc0032344231570a20202020202020000000fd00324c1e5311000a202020202020011802031ef14b101f051404130312021101230907078301000065030c0010008c0ad08a20e02d10103e9600092521000018011d007251d01e206e28550009252100001e8c0ad08a20e02d10103e96000925210000188c0ad090204031200c40550009252100001800000000000000000000000000000000000000000000000000f1";
                 DP-2-3 = "00ffffffffffff0005e30124a96107001f1e010368341d782a9b15a655519d260d5054bfef00d1c0b30095008180814081c001010101023a801871382d40582c450009252100001e000000ff00474d584c384841343833373533000000fc0032344231570a20202020202020000000fd00324c1e5311000a202020202020001b";
                 eDP-1 = "00ffffffffffff000daec91400000000081a0104951f11780228659759548e271e505400000001010101010101010101010101010101b43b804a71383440503c680035ad10000018000000fe004e3134304843412d4541420a20000000fe00434d4e0a202020202020202020000000fe004e3134304843412d4541420a20003e";
               };
@@ -94,21 +94,21 @@ in
                   enable = true;
                   crtc = 2;
                   mode = "1920x1080";
-                  position  = "0x0";
+                  position = "0x0";
                   rate = "60.00";
                 };
                 DP-2-2 = {
                   enable = true;
                   crtc = 1;
                   mode = "1920x1080";
-                  position  = "1920x0";
+                  position = "1920x0";
                   rate = "60.00";
                 };
                 eDP-1 = {
                   enable = true;
                   crtc = 0;
                   mode = "1920x1080";
-                  position  = "3840x0";
+                  position = "3840x0";
                   rate = "60.01";
                 };
               };
@@ -170,14 +170,14 @@ in
           # Htop configurations
           htop = {
             enable = true;
-            hideUserlandThreads = true;
-            highlightBaseName = true;
-            shadowOtherUsers = true;
-            showProgramPath = false;
-            treeView = false;
-            meters = {
-              left = [ "LeftCPUs" "Memory" "Swap" "ZFSARC"];
-              right = [ "RightCPUs" "Tasks" "LoadAverage" "Uptime" "Battery" ];
+            settings = {
+              hide_userland_threads = true;
+              highlight_base_name = true;
+              shadow_other_users = true;
+              show_program_path = false;
+              tree_view = false;
+              left_meters = [ "LeftCPUs" "Memory" "Swap" "ZFSARC" ];
+              right_meters = [ "RightCPUs" "Tasks" "LoadAverage" "Uptime" "Battery" ];
             };
           };
 
