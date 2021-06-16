e0fd05adc134cb353908d6ac3463e4943bc524ac
System administrator <asdf@ragon.xyz>
2021-06-16 22:12:17 +0200

    wormhole pubkey

--- data/pubkeys.nix
+++ data/pubkeys.nix
@@ -11,12 +11,14 @@ let
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCScEol5a8Gh1P0t7w3FNjG6V9XEDcH2sJWgPJ41BpQ2Y70CKJ/ipcuzMoZLkidoeEuqaZXfQuv90acKbCJd7k0eEjpvOIpim0K/K7K+hajzkckEETaWLShjiHpYJMuDFJKLl+Lx3YjLbSIyUKDLtDeGfqj9G8/PjhiQnjNIkPXyqdVZJG7hEOcxzUKA6JkrOk+Okg7JSF8ZBCMDI//7EyctCP5gTJNdaN5GR2JL33Iyk/A+PngM6Lxjlu2oY+5vts2X6ntFEBFaktpPSCgqXTfGW5RefLEqFCReGAD3KYa5dmWkU431K7E3GqwiCyxcV9C5bDRYJne6Txf092D1dDKo+zCoTzi39y0TUMoVLwNARLYM4CW4uIZcMGYDqYEzPxU0NkrbU8i32zZ2FGoHBMbPhgecm+v6qxZbCKWNM/vaY+qK9fW5uDyktnOyeaFj0XeSV3udO/kLt2yYuRejv9Uc72WAzuNG7ouFEh8uyDalLZgouY5YBJHvaRjVg/oBTs=" # root
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXtH/ZY7u7ejf+EyzWleWRVUP8aNU5Gna5lpfVPRcuj" # enterprise hostkey
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKejPH5g1z8Syx5YhypidjMZ6itJTgDBBpfAVUIb4+a5" # voyager hostkey
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7Ab/50Bg9EncYbobC6kzdDzCDgvM1Tlf/9d+SDkO15" # wormhole hostkey
 
-      ];
+      ] ++ hosts.wormhole;
+      hosts = {
+        wormhole = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7Ab/50Bg9EncYbobC6kzdDzCDgvM1Tlf/9d+SDkO15"]; # wormhole hostkey
+      };
     in
     {
-      inherit pc work;
+      inherit pc work hosts;
       computers = pc ++ work ++ host;
 
     };
