0cddaf9a9330ecfd580cc0da3c00a62c9e142300
Philipp Hochkamp <me@phochkamp.de>
2021-06-21 01:53:47 +0200

    ﻿ - Temporary commit.

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -8,10 +8,10 @@ in
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
     systemd.user.services.mpris-proxy = {
-      Unit.Description = "Mpris proxy";
-      Unit.After = [ "network.target" "sound.target" ];
-      Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-      Install.WantedBy = [ "default.target" ];
+      description = "Mpris proxy";
+      after = [ "network.target" "sound.target" ];
+      execStart = "${pkgs.bluez}/bin/mpris-proxy";
+      wantedBy = [ "default.target" ];
     };
     hardware.pulseaudio = {
       extraModules = [ pkgs.pulseaudio-modules-bt ];
