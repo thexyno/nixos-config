f5c92b05d20d07ba426c046baf0592dfb46057da
Philipp Hochkamp <me@phochkamp.de>
2021-07-08 03:47:16 +0200

    fix: pulse_launch

--- packages/pulse_launch.nix
+++ packages/pulse_launch.nix
@@ -1,17 +1,23 @@
-{ lib, stdenv, fetchFromGitHub, python3Packages }:
-with lib;
+{ lib, stdenv, fetchFromGitHub, python3Packages, python3 }:
+let
+  py = python3.withPackages (pythonPackages: with pythonPackages; [ pulsectl ]);
+in
 stdenv.mkDerivation rec {
   version = "1.0.0";
   pname = "pulse_launch";
-  propagatedBuildInputs = with python3Packages; [ pulsectl ];
+  format = "other";
+  doCheck = false;
   src = fetchFromGitHub {
     owner = "ragon000";
     repo = "pulse-launch";
     rev = "bc5910eb3fcfa933a5ff84472948e70a92eff7b4";
     sha256 = "11n9mz8gs3zklm818a6g80g9panbz58q9qjh6zwmnrwvgnzf13wc";
   };
+  buildPhase = ''
+    echo "#!${py}/bin/python3" | cat - ${pname}.py > ${pname}
+  '';
   installPhase = ''
-    install -D -m 755 ${pname}.py $out/bin/${pname}
+    install -D -m 755 ${pname} $out/bin/${pname}
   '';
   meta = with lib; {
     description = "A simple python script to launch a command when a pulseaudio sink changes";
