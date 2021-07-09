8be8141823f568f3cc69ef108c4429befd42cb0b
Philipp Hochkamp <me@phochkamp.de>
2021-07-09 15:40:03 +0200

    fix: iso

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -4,10 +4,12 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
     "${modulesPath}/installer/cd-dvd/channel.nix"
   ];
-  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
   fileSystems."/".device = "tmpfs";
+  users.users.root.password = "123456";
+  services.sshd.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvexOT9tnx2LfAE/OwfixfNc/esNAjZ+GDfLpY2iABk philipp@philipp-archPC"
   ];
+  networking.firewall.enable = false;
 
 } 
