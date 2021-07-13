d1f713fc02a72d3e2aedc5f79dd963486e53117e
Philipp Hochkamp <me@phochkamp.de>
2021-07-13 15:45:00 +0200

    feat: more pubkeys

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, modulesPath, ...}:
+{ config, pkgs, modulesPath, ... }:
 {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
@@ -7,9 +7,7 @@
   fileSystems."/".device = "tmpfs";
   users.users.root.password = "123456";
   services.sshd.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvexOT9tnx2LfAE/OwfixfNc/esNAjZ+GDfLpY2iABk philipp@philipp-archPC"
-  ];
+  users.users.root.openssh.authorizedKeys.keys = pkgs.pubkeys.ragon.computers;
   networking.firewall.enable = false;
 
 } 
