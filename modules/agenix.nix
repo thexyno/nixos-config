a822f3a32114dd91ff8e3f9545d8d64b093af69a
Philipp Hochkamp <me@phochkamp.de>
2021-07-13 16:33:52 +0200

    more agenix fixes

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -11,28 +11,26 @@ let
 in
 {
   options.ragon.agenix.enable = mkBoolOpt true;
-  config = mkIf cfg.enable {
-    imports = [ agenix.nixosModules.age ];
-    environment.systemPackages = [ agenix.defaultPackage.${pkgs.system} ];
-    # Set passwords
-    users.users.root.passwordFile = "${age.secrets.rootPasswd.path}";
+  imports = [ agenix.nixosModules.age ];
+  environment.systemPackages = [ agenix.defaultPackage.${pkgs.system} ];
+  # Set passwords
+  users.users.root.passwordFile = "${age.secrets.rootPasswd.path}";
 
-    age = {
-      secrets =
-        if pathExists secretsFile
-        then
-          mapAttrs'
-            (n: _: nameValuePair (removeSuffix ".age" n) {
-              file = "${secretsDir}/${n}";
-              owner = if (hasInfix "root" n) then mkDefault "root" else mkDefault config.ragon.user.username;
-            })
-            (import secretsFile)
-        else { };
-      sshKeyPaths =
-        [
-          "/persistent/etc/ssh/ssh_host_rsa_key"
-          "/persistent/etc/ssh/ssh_host_ed25519_key"
-        ];
-    };
+  age = {
+    secrets =
+      if (pathExists secretsFile && cfg.enable)
+      then
+        mapAttrs'
+          (n: _: nameValuePair (removeSuffix ".age" n) {
+            file = "${secretsDir}/${n}";
+            owner = if (hasInfix "root" n) then mkDefault "root" else mkDefault config.ragon.user.username;
+          })
+          (import secretsFile)
+      else { };
+    sshKeyPaths =
+      [
+        "/persistent/etc/ssh/ssh_host_rsa_key"
+        "/persistent/etc/ssh/ssh_host_ed25519_key"
+      ];
   };
 }
