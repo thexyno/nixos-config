441f5edabe6a35c9c43245d7d284848aab51a388
Philipp Hochkamp <me@phochkamp.de>
2021-07-22 03:39:32 +0200

    renaming

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -7,12 +7,12 @@ in
   "nextshot.age".publicKeys = pubkeys.ragon.computers;
   "pulseLaunch.age".publicKeys = pubkeys.ragon.computers;
   "rootPasswd.age".publicKeys = pubkeys.ragon.computers;
-  "rootRagonPasswd.age".publicKeys = pubkeys.ragon.computers;
+  "ragonPasswd.age".publicKeys = pubkeys.ragon.computers;
   "gitlabInitialRootPassword.age".publicKeys = pubkeys.ragon.computers;
   "gitlabSecretFile.age".publicKeys = pubkeys.ragon.computers;
   "gitlabDBFile.age".publicKeys = pubkeys.ragon.computers;
   "gitlabOTPFile.age".publicKeys = pubkeys.ragon.computers;
   "gitlabJWSFile.age".publicKeys = pubkeys.ragon.computers;
   "nextcloudAdminPass.age".publicKeys = pubkeys.ragon.computers;
-  "rootWireguardwormhole.age".publicKeys = pubkeys.ragon.hosts.wormhole;
+  "wireguardwormhole.age".publicKeys = pubkeys.ragon.hosts.wormhole;
 }
