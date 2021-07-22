d173e13d1a407df466a24e75d52cb78dc2a6b4b2
Philipp Hochkamp <me@phochkamp.de>
2021-07-22 09:17:42 +0200

    doh.

--- hosts/kitchen/default.nix
+++ hosts/kitchen/default.nix
@@ -9,7 +9,7 @@
     version = 3;
   };
   documentation.enable = false;
-  boot.kernelPackages = pkgs.linux_rpi3;
+  #boot.kernelPackages = pkgs.linux_rpi3;
   boot.extraModprobeConfig = ''
     options cfg80211 ieee80211_regdom="DE"
   '';
