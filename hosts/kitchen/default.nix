b4b43bb17b57ae5062c01d0b22ac1791662421cd
Philipp Hochkamp <me@phochkamp.de>
2021-07-22 09:16:19 +0200

    pi: enable sound?

--- hosts/kitchen/default.nix
+++ hosts/kitchen/default.nix
@@ -3,6 +3,7 @@
   imports = [
     "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
   ];
+  sound.enable = true;
   boot.loader.raspberryPi = {
     enable = true;
     version = 3;
