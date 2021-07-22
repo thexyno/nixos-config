ef7f4aaba1d745539ebbd2784a63451474d0815b
Philipp Hochkamp <me@phochkamp.de>
2021-07-22 05:32:00 +0200

    idk

--- hosts/kitchen/default.nix
+++ hosts/kitchen/default.nix
@@ -68,4 +68,20 @@
   networking.wireless.enable = true;
   hardware.enableRedistributableFirmware = true;
   networking.wireless.interfaces = [ "wlan0" ];
+
+nixpkgs.overlays = [
+    (self: super: {
+      firmwareLinuxNonfree = super.firmwareLinuxNonfree.overrideAttrs (old: {
+        version = "2020-12-18";
+        src = pkgs.fetchgit {
+          url =
+            "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
+          rev = "b79d2396bc630bfd9b4058459d3e82d7c3428599";
+          sha256 = "1rb5b3fzxk5bi6kfqp76q1qszivi0v1kdz1cwj2llp5sd9ns03b5";
+        };
+        outputHash = "1p7vn2hfwca6w69jhw5zq70w44ji8mdnibm1z959aalax6ndy146";
+      });
+    })
+  ];
+
 }
