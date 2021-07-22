17c804cf84706ca7e45d9c519e39ade9ae3e85d5
Philipp Hochkamp <me@phochkamp.de>
2021-07-22 08:58:32 +0200

    try fix rpi audio

--- hosts/kitchen/default.nix
+++ hosts/kitchen/default.nix
@@ -45,7 +45,7 @@
   };
 
   boot.loader.raspberryPi.firmwareConfig = ''
-    dtoverlay=hifiberry-amp
+    dtoverlay=hifiberry-dac
   '';
   hardware.pulseaudio.extraConfig = ''
     unload-module module-native-protocol-unix
@@ -75,19 +75,19 @@
   hardware.enableRedistributableFirmware = true;
   networking.wireless.interfaces = [ "wlan0" ];
 
-nixpkgs.overlays = [
-    (self: super: {
-      firmwareLinuxNonfree = super.firmwareLinuxNonfree.overrideAttrs (old: {
-        version = "2020-12-18";
-        src = pkgs.fetchgit {
-          url =
-            "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-          rev = "b79d2396bc630bfd9b4058459d3e82d7c3428599";
-          sha256 = "1rb5b3fzxk5bi6kfqp76q1qszivi0v1kdz1cwj2llp5sd9ns03b5";
-        };
-        outputHash = "1p7vn2hfwca6w69jhw5zq70w44ji8mdnibm1z959aalax6ndy146";
-      });
-    })
-  ];
+  #nixpkgs.overlays = [
+  #  (self: super: {
+  #    firmwareLinuxNonfree = super.firmwareLinuxNonfree.overrideAttrs (old: {
+  #      version = "2020-12-18";
+  #      src = pkgs.fetchgit {
+  #        url =
+  #          "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
+  #        rev = "b79d2396bc630bfd9b4058459d3e82d7c3428599";
+  #        sha256 = "1rb5b3fzxk5bi6kfqp76q1qszivi0v1kdz1cwj2llp5sd9ns03b5";
+  #      };
+  #      outputHash = "1p7vn2hfwca6w69jhw5zq70w44ji8mdnibm1z959aalax6ndy146";
+  #    });
+  #  })
+  #];
 
 }
