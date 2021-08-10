e85a8250385f1df09f9086bcb164443880607f44
Philipp Hochkamp <me@phochkamp.de>
2021-08-10 20:09:51 +0200

    added grocy

---
+++ modules/services/grocy.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.ragon.services.grocy;
+  domain = config.ragon.services.nginx.domain;
+in
+{
+  options.ragon.services.grocy.enable = lib.mkEnableOption "Enables grocy";
+  options.ragon.services.grocy.domainPrefix =
+    lib.mkOption {
+      type = lib.types.str;
+      default = "grocy";
+    };
+  config = lib.mkIf cfg.enable {
+    services.nginx.virtualHosts."${cfg.domainPrefix}.${domain}" = { # all other settings get set by services.grocy
+      forceSSL = true;
+      useACMEHost = "${domain}";
+    };
+    services.grocy = {
+      enable = true;
+      hostName = "${cfg.domainPrefix}.${domain}";
+      settings = {
+        currency = "EUR";
+        culture = "de";
+        firstDayOfWeek = 1;
+      };
+    };
+    ragon.persist.extraDirectories = [
+      "${config.services.grocy.dataDir}"
+    ];
+  };
+}
