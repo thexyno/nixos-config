7e673168192defd95cf2dc29db04cf0d9c140175
Philipp Hochkamp <me@phochkamp.de>
2021-08-13 17:41:07 +0200

    pgsql is more strict, increase the hackiness up to 11

--- modules/services/mullvad.nix
+++ modules/services/mullvad.nix
@@ -5,7 +5,9 @@ in
 {
   options.ragon.services.mullvad.enable = lib.mkEnableOption "Enables the mullvad Client service";
   config = lib.mkIf cfg.enable {
-    # \ragon a bit sparse for now
+    # https://github.com/NixOS/nixpkgs/issues/113589#issuecomment-893233499
+    networking.firewall.checkReversePath = "loose";
+    networking.wireguard.enable = true;
     services.mullvad-vpn.enable = true;
     environment.systemPackages = [ pkgs.mullvad-vpn ];
   };
