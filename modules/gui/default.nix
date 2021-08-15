1695e9cd3b4c811b5fff3475c2655ddf43fabb2d
Philipp Hochkamp <me@phochkamp.de>
2021-08-15 17:46:59 +0200

    ALL SORTS OF THINGS

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -49,7 +49,7 @@ in
     # Install some extra fonts.
     fonts.fonts = with pkgs; [
       jetbrains-mono
-      nerdfonts
+      (nerdfonts.override { fonts = [ "JetBrainsMono" "Terminus"])
     ];
 
 
