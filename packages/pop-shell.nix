d06a670c7ead527ea9a001e03bd3fb1e8f2a52cc
Philipp Hochkamp <me@phochkamp.de>
2021-08-22 17:56:50 +0200

    pgsql is being a pain

---
+++ packages/pop-shell.nix
@@ -0,0 +1,30 @@
+{ stdenv, inputs, fetchFromGitHub, nodePackages, glib }:
+stdenv.mkDerivation rec {
+  pname = "gnome-shell-extension-pop-shell";
+  version = "2020-03-16";
+
+  src = inputs.pop-shell;
+
+  nativeBuildInputs = [
+    nodePackages.typescript
+    glib
+  ];
+
+  makeFlags = [ "INSTALLBASE=$(out)/share/gnome-shell/extensions" ];
+
+  postInstall = ''
+    mkdir -p $out/share/gsettings-schemas/pop-shell-${version}/glib-2.0
+    cp -R $out/share/gnome-shell/extensions/${uuid}/schemas \
+          $out/share/gsettings-schemas/pop-shell-${version}/glib-2.0/schemas
+  '';
+
+  uuid = "pop-shell@system76.com";
+
+  meta = with stdenv.lib; {
+    description = "i3wm-like keyboard-driven layer for GNOME Shell";
+    homepage = "https://github.com/pop-os/shell";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ elyhaka ];
+    platforms = platforms.linux;
+  };
+}
