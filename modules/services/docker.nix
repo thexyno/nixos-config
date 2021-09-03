5fa19fce197351075d621161a31a70ea5fb23832
Philipp Hochkamp <me@phochkamp.de>
2021-09-03 20:35:50 +0200

    A fix I believe, not like I tested or anything

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -7,13 +7,12 @@ in
   config = lib.mkIf cfg.enable {
     virtualisation.oci-containers.backend = "podman";
     virtualisation.podman.enable = true;
+    virtualisation.podman.defaultNetwork.dnsname.enable = true;
     virtualisation.podman.dockerCompat = true;
     ragon.user.extraGroups = [ "docker" "podman" ];
     ragon.user.persistent.extraDirectories = [ ".local/share/containers" ".cache/containers" ];
-    ragon.persist.extraDirectories = [ "/var/lib/containers" ];
     virtualisation.containers.storage.settings.storage = {
       driver = "zfs";
-      mount_program = "${pkgs.zfs}/bin/mount.zfs";
       options.zfs.fsname = "pool/containers";
     };
   };
