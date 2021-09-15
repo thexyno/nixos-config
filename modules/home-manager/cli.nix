2c9cab15c0c10c3f29854b4c05a637441eab0b0c
Philipp Hochkamp <me@phochkamp.de>
2021-09-15 20:12:14 +0200

    Derp search/replace fuckup

--- modules/home-manager/cli.nix
+++ modules/home-manager/cli.nix
@@ -5,9 +5,40 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
+    ragon.user.persistent.extraDirectories = [
+      ".gnupg"
+    ];
     home-manager.users.${config.ragon.user.username} = { pkgs, lib, ... }:
       {
         programs = {
+          gpg = {
+            enable = true;
+            settings = {
+              personal-cipher-preferences = "AES256 AES192 AES";
+              personal-digest-preferences = "SHA512 SHA384 SHA256";
+              personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+              default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+              cert-digest-algo = "SHA512";
+              s2k-digest-algo = "SHA512";
+              s2k-cipher-algo = "AES256";
+              charset = "utf-8";
+              fixed-list-mode = true;
+              no-comments = true;
+              no-emit-version = true;
+              no-greeting = true;
+              list-options = "show-uid-validity";
+              verify-options = "show-uid-validity";
+              with-fingerprint = true;
+              with-key-origin = true;
+              require-cross-certification = true;
+              no-symkey-cache = true;
+              use-agent = true;
+              throw-keyids = true;
+              keyserver = "hkps://keyserver.ubuntu.com:443";
+              verbose = true;
+              list-options = "show-unusable-subkeys";
+            };
+          };
           bat = {
             enable = true;
             config.theme = "gruvbox-dark";
