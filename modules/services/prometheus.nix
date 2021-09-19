e514cd63dc0bacd2f2b19877a92772d6387aa59e
Philipp Hochkamp <me@phochkamp.de>
2021-09-19 02:23:43 +0200

    prometheus

--- modules/services/prometheus.nix
+++ modules/services/prometheus.nix
@@ -2,30 +2,52 @@
 with lib;
 with lib.my;
 let
-  cfg = builtins.importTOML ../../data/prometheus.toml;
+  cfg = importTOML ../../data/prometheus.toml;
   hostName = config.networking.hostName;
 in
 {
-  config = traceValSeq ( mkMerge [
-    (mkIf (cfg.master.hostaname == hostName) {
+  config = mkMerge ([
+    (mkIf (cfg.master.hostname == hostName) {
       services.prometheus = {
         enable = true;
-        port = 9001;
+        port = cfg.master.port;
+        scrapeConfigs = traceValSeq (foldl (a: b: a ++ b) [] (map (x: (map (y: {
+          job_name = "${x}_${y}";
+          static_configs = [
+            {
+              targets = [
+                ''${
+                  if (y == hostName)
+                  then "127.0.0.1"
+                  else (
+                    if (builtins.elem y (builtins.attrNames cfg.hostOverrides))
+                    then cfg.hostOverrides.${y}
+                    else "${y}.hailsatan.eu"
+                  )
+                }:${toString cfg.exporters.${x}.port}''
+              ];
+            }
+          ];
+        }) cfg.exporters.${x}.hosts )) (builtins.attrNames cfg.exporters)));
       };
       ragon.persist.extraDirectories = [
         "/usr/lib/${config.services.prometheus.stateDir}"
       ];
     })
-      map (x: {
-        services.prometheus.exporters.${x}.enable = builtins.elem hostName cfg.exporters.${x}.hosts;
-        services.prometheus.exporters.${x}.port = cfg.exporters.${x}.port;
-        services.prometheus.exporters.${x}.openFirewall = true;
-        services.prometheus.exporters.${x}.firewallFilter = "-p tmp -s ${cfg.master.ip} -m tcp --dport ${toString cfg.exporters.${x}.port}";
-      } ) (builtins.attrNames cfg.exporters)
-      ({
+      {
         # some global settings
         services.prometheus.exporters.node.enabledCollectors = [ "systemd" ];
-      })
-  ]);
+      }
+    ] ++
+    (map (x: {
+      services.prometheus.exporters.${x} = {
+        enable = (builtins.elem hostName cfg.exporters.${x}.hosts);
+        port = cfg.exporters.${x}.port;
+        openFirewall = true;
+        firewallFilter = "-p tmp -s ${cfg.master.ip} -m tcp --dport ${toString cfg.exporters.${x}.port}";
+      };
+      } ) (builtins.attrNames cfg.exporters))
+      );
+
 }
 
