05e26b8e0c614c0a21de16857e77423ec03cc585
Philipp Hochkamp <me@phochkamp.de>
2021-09-23 11:43:37 +0200

    Version control is awful

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -10,5 +10,46 @@
   users.users.root.openssh.authorizedKeys.keys = pkgs.pubkeys.ragon.computers;
   networking.firewall.enable = false;
   ragon.agenix.enable = false;
+  environment.systemPackages = [
+    (pkgs.writeScript "generateSystem1" ''
+      #!${pkgs.bash}/bin/bash
+      set -x
+      if [ -z $1 ]; then
+        echo please set a device
+        exit 1
+      fi
+      if [ -z $2 ]; then
+        echo please set a swap size
+        exit 1
+      fi
+      dev=/dev/$1
+      parted $dev -- mklabel gpt
+      parted $dev -- mkpart primary 512M -$2
+      parted $dev -- mkpart primary -$2 100%
+      parted $dev -- mkpart primary esp 1M 512M
+      mkfs.fat -F32 -n boot ''${dev}3
+      mount -t tmpfs tmpfs /mnt
+      mkdir -p /mnt/{boot,nix,persist,etc/ssh,var/{lib,log}}
+      echo now create your main file system (on ''${dev}1), mount /mnt/nix and /mnt/persist and then run generateSystem2
+    '')
+    (pkgs.writeScript "generateSystem2" ''
+      #!${pkgs.bash}/bin/bash
+      set -x
+      if [ -z $1 ]; then
+        echo please set a device
+        exit 1
+      fi
+      dev=/dev/$1
+      
+      mkdir -p /mnt/persist/{etc/ssh,var/{lib,log},srv}
+      mount -o bind /mnt/persist/var/log /mnt/var/log
+      ssh-keygen -t ed25519 -f /mnt/persist/etc/ssh/ssh_host_ed25519_key
+      ssh-keygen -t rsa -f /mnt/persist/etc/ssh/ssh_host_rsa_key
+      echo now add this pubkey to your agenix please
+      cat /mnt/persist/etc/ssh/ssh_host_ed25519_key.pub
+
+      echo then you can install your system with "nixos-install --root /mnt --no-root-passwd --flake github:ragon000/nixos-config#<hostname>"
+    '')
+  ];
 
 } 
