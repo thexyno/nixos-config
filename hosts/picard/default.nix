2b71fdb0f347a8b95d6f86e07b8983318c1373e5
Philipp Hochkamp <me@phochkamp.de>
2021-10-19 02:11:09 +0200

    picard home manager

--- hosts/picard/default.nix
+++ hosts/picard/default.nix
@@ -34,9 +34,12 @@
   # Immutable users due to tmpfs
   users.mutableUsers = false;
 
+  services.postgresql.package = pkgs.postgresql_13;
+
   ragon = {
     cli.enable = true;
     user.enable = true;
+    home-manager.enable = true;
     persist.enable = true;
 
     services = {
@@ -50,36 +53,5 @@
     };
 
   };
-
-  containers.temp-pg.config.services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_13;
-    ## set a custom new dataDir
-    # dataDir = "/some/data/dir";
-  };
-  services.postgresql.package = pkgs.postgresql_13;
-  environment.systemPackages =
-    let newpg = config.containers.temp-pg.config.services.postgresql;
-    in
-    [
-      (pkgs.writeScriptBin "upgrade-pg-cluster" ''
-        set -x
-        export OLDDATA="${config.services.postgresql.dataDir}"
-        export NEWDATA="${newpg.dataDir}"
-        export OLDBIN="${config.services.postgresql.package}/bin"
-        export NEWBIN="${newpg.package}/bin"
-
-        install -d -m 0700 -o postgres -g postgres "$NEWDATA"
-        cd "$NEWDATA"
-        sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
-
-        systemctl stop postgresql    # old one
-
-        sudo -u postgres $NEWBIN/pg_upgrade \
-          --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
-          --old-bindir $OLDBIN --new-bindir $NEWBIN \
-          "$@"
-      '')
-    ];
 
 }
