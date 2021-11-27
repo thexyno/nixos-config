63c623828a0a52d7be3d40429bad2a05519e5351
Philipp Hochkamp <git@phochkamp.de>
2021-11-28 00:19:48 +0100

    rip wormhole sd

--- data/pubkeys.nix
+++ data/pubkeys.nix
@@ -9,7 +9,7 @@ let
       server = user ++ hosts.ds9 ++ hosts.wormhole ++ hosts.picard;
       client = user ++ hosts.enterprise ++ hosts.voyager;
       hosts = {
-        wormhole = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7Ab/50Bg9EncYbobC6kzdDzCDgvM1Tlf/9d+SDkO15" ];
+        wormhole = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzR5dq/2UosH3nLrc9PvJi3rzX917K2wICeOUAiDnl6" ];
         enterprise = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXtH/ZY7u7ejf+EyzWleWRVUP8aNU5Gna5lpfVPRcuj" ];
         voyager = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKejPH5g1z8Syx5YhypidjMZ6itJTgDBBpfAVUIb4+a5" ];
         ds9 = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+BBXufXAJhyUHVfhqlk8Y4zEKJbKXgJQvsdE482lpV" ];
