5d68025e4a258c52d01745cd3b5840e796e27a3e
Philipp Hochkamp <git@phochkamp.de>
2021-12-02 01:03:16 +0100

    fix: secrets

--- data/pubkeys.nix
+++ data/pubkeys.nix
@@ -6,7 +6,7 @@ let
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJwOH1b6xWmEr1VZh48kBIYhW11vtPFR3my8stAHlSi" # saurier
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwdsCeQUDRkAF/9Pfs5vySEit4kd5XGCgGR+CfZStAz" # voyager
       ];
-      server = user ++ hosts.ds9 ++ hosts.wormhole ++ hosts.picard;
+      server = user ++ hosts.ds9 ++ hosts.wormhole ++ hosts.picard ++ hosts.octopi;
       client = user ++ hosts.enterprise ++ hosts.voyager;
       hosts = {
         wormhole = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzR5dq/2UosH3nLrc9PvJi3rzX917K2wICeOUAiDnl6" ];
@@ -15,12 +15,14 @@ let
         ds9 = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+BBXufXAJhyUHVfhqlk8Y4zEKJbKXgJQvsdE482lpV" ];
         picard = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAm3UXZaN95WBUaS9SiHLNEuI1tP1x1w07qnYxPe+vdr" ];
         odyssey = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGJ/AeaPwEblvpbxx5RGc9205H3eytJusLDJKN63LiJ" ];
+        octopi = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRhKTcmYaM0yNYScEUBP8WV56MBvmLzXhJXcbDTGeea ragon@enterprise" ];
       };
     in
     {
-      inherit user server client hosts;
+      inherit user server client;
       computers = user ++ (builtins.foldl' (a: b: a ++ b) [ ] (builtins.attrValues hosts)); # everything
       host = hn: (hosts.${hn} ++ user);
+      hosts = hn: ((map (x: hosts.${x}) hn) ++ user);
     };
 in
 {
