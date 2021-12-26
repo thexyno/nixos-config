a44667e2ee4661897a00c45db62ec93b1d48b701
Philipp Hochkamp <git@phochkamp.de>
2021-12-26 21:13:11 +0100

    tm-fix

--- hosts/ds9/default.nix
+++ hosts/ds9/default.nix
@@ -68,19 +68,19 @@
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
   services.avahi.publish.enable = true;
-  services.avahi.extraServiceFiles.adisk = ''
-    <?xml version="1.0" standalone='no'?>
-    <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-    <service-group>
-      <name replace-wildcards="yes">%h</name>
-      <service>
-        <type>_adisk._tcp</type>
-        <port>9</port>
-        <txt-record>sys=waMa=0,adVF=0x100</txt-record>
-        <txt-record>dk0=adVN=daedalusTM,adVF=0x82</txt-record>
-      </service>
-    </service-group>
-  '';
+  # services.avahi.extraServiceFiles.adisk = ''
+  #   <?xml version="1.0" standalone='no'?>
+  #   <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+  #   <service-group>
+  #     <name replace-wildcards="yes">%h</name>
+  #     <service>
+  #       <type>_adisk._tcp</type>
+  #       <port>9</port>
+  #       <txt-record>sys=waMa=0,adVF=0x100</txt-record>
+  #       <txt-record>dk0=adVN=daedalusTM,adVF=0x82</txt-record>
+  #     </service>
+  #   </service-group>
+  # '';
   services.avahi.extraServiceFiles.smb = ''
     <?xml version="1.0" standalone='no'?>
     <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
