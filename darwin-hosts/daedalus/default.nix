9de88b1e25bab04a3dbe08b4b6abcd4e746c4cab
Philipp Hochkamp <git@phochkamp.de>
2022-02-19 16:05:39 +0100

    tmux

--- darwin-hosts/daedalus/default.nix
+++ darwin-hosts/daedalus/default.nix
@@ -56,6 +56,7 @@ with lib.my;
             };
           in
           map (x: { plugin = x; }) (with pkgs.vimPlugins; [
+            vim-tmux-navigator
             galaxyline-nvim
             nvim-web-devicons
             nnn-vim
@@ -120,7 +121,6 @@ with lib.my;
       lefthook
       yt-dlp
       aria2
-      tmux
       libqalculate
 
 
@@ -145,118 +145,77 @@ with lib.my;
 
       google-cloud-sdk
     ];
-    programs.alacritty = {
-      enable = true;
-      settings = {
-        key_bindings = [
-          { key = "A"; mods = "Option"; chars = "\\x1ba"; } # Alt settings ref = "https://github.com/alacritty/alacritty/issues/62#issuecomment-347552058
-          { key = "B"; mods = "Option"; chars = "\\x1bb"; }
-          { key = "C"; mods = "Option"; chars = "\\x1bc"; }
-          { key = "D"; mods = "Option"; chars = "\\x1bd"; }
-          { key = "E"; mods = "Option"; chars = "\\x1be"; }
-          { key = "F"; mods = "Option"; chars = "\\x1bf"; }
-          { key = "G"; mods = "Option"; chars = "\\x1bg"; }
-          { key = "H"; mods = "Option"; chars = "\\x1bh"; }
-          { key = "I"; mods = "Option"; chars = "\\x1bi"; }
-          { key = "J"; mods = "Option"; chars = "\\x1bj"; }
-          { key = "K"; mods = "Option"; chars = "\\x1bk"; }
-          { key = "L"; mods = "Option"; chars = "\\x1bl"; }
-          { key = "M"; mods = "Option"; chars = "\\x1bm"; }
-          { key = "N"; mods = "Option"; chars = "\\x1bn"; }
-          { key = "O"; mods = "Option"; chars = "\\x1bo"; }
-          { key = "P"; mods = "Option"; chars = "\\x1bp"; }
-          { key = "Q"; mods = "Option"; chars = "\\x1bq"; }
-          { key = "R"; mods = "Option"; chars = "\\x1br"; }
-          { key = "S"; mods = "Option"; chars = "\\x1bs"; }
-          { key = "T"; mods = "Option"; chars = "\\x1bt"; }
-          { key = "U"; mods = "Option"; chars = "\\x1bu"; }
-          { key = "V"; mods = "Option"; chars = "\\x1bv"; }
-          { key = "W"; mods = "Option"; chars = "\\x1bw"; }
-          { key = "X"; mods = "Option"; chars = "\\x1bx"; }
-          { key = "Y"; mods = "Option"; chars = "\\x1by"; }
-          { key = "Z"; mods = "Option"; chars = "\\x1bz"; }
-          { key = "A"; mods = "Option|Shift"; chars = "\\x1bA"; }
-          { key = "B"; mods = "Option|Shift"; chars = "\\x1bB"; }
-          { key = "C"; mods = "Option|Shift"; chars = "\\x1bC"; }
-          { key = "D"; mods = "Option|Shift"; chars = "\\x1bD"; }
-          { key = "E"; mods = "Option|Shift"; chars = "\\x1bE"; }
-          { key = "F"; mods = "Option|Shift"; chars = "\\x1bF"; }
-          { key = "G"; mods = "Option|Shift"; chars = "\\x1bG"; }
-          { key = "H"; mods = "Option|Shift"; chars = "\\x1bH"; }
-          { key = "I"; mods = "Option|Shift"; chars = "\\x1bI"; }
-          { key = "J"; mods = "Option|Shift"; chars = "\\x1bJ"; }
-          { key = "K"; mods = "Option|Shift"; chars = "\\x1bK"; }
-          { key = "L"; mods = "Option|Shift"; chars = "\\x1bL"; }
-          { key = "M"; mods = "Option|Shift"; chars = "\\x1bM"; }
-          { key = "N"; mods = "Option|Shift"; chars = "\\x1bN"; }
-          { key = "O"; mods = "Option|Shift"; chars = "\\x1bO"; }
-          { key = "P"; mods = "Option|Shift"; chars = "\\x1bP"; }
-          { key = "Q"; mods = "Option|Shift"; chars = "\\x1bQ"; }
-          { key = "R"; mods = "Option|Shift"; chars = "\\x1bR"; }
-          { key = "S"; mods = "Option|Shift"; chars = "\\x1bS"; }
-          { key = "T"; mods = "Option|Shift"; chars = "\\x1bT"; }
-          { key = "U"; mods = "Option|Shift"; chars = "\\x1bU"; }
-          { key = "V"; mods = "Option|Shift"; chars = "\\x1bV"; }
-          { key = "W"; mods = "Option|Shift"; chars = "\\x1bW"; }
-          { key = "X"; mods = "Option|Shift"; chars = "\\x1bX"; }
-          { key = "Y"; mods = "Option|Shift"; chars = "\\x1bY"; }
-          { key = "Z"; mods = "Option|Shift"; chars = "\\x1bZ"; }
-          { key = "Key1"; mods = "Option"; chars = "\\x1b1"; }
-          { key = "Key2"; mods = "Option"; chars = "\\x1b2"; }
-          { key = "Key3"; mods = "Option"; chars = "\\x1b3"; }
-          { key = "Key4"; mods = "Option"; chars = "\\x1b4"; }
-          { key = "Key5"; mods = "Option"; chars = "\\x1b5"; }
-          { key = "Key6"; mods = "Option"; chars = "\\x1b6"; }
-          { key = "Key7"; mods = "Option"; chars = "\\x1b7"; }
-          { key = "Key8"; mods = "Option"; chars = "\\x1b8"; }
-          { key = "Key9"; mods = "Option"; chars = "\\x1b9"; }
-          { key = "Key0"; mods = "Option"; chars = "\\x1b0"; }
-          { key = "Space"; mods = "Control"; chars = "\\x00"; } # Ctrl + Space
-          { key = "Grave"; mods = "Option"; chars = "\\x1b`"; } # Alt + `
-          { key = "Grave"; mods = "Option|Shift"; chars = "\\x1b~"; } # Alt + ~
-          { key = "Period"; mods = "Option"; chars = "\\x1b."; } # Alt + .
-          { key = "Key8"; mods = "Option|Shift"; chars = "\\x1b*"; } # Alt + *
-          { key = "Key3"; mods = "Option|Shift"; chars = "\\x1b#"; } # Alt + #
-          { key = "Period"; mods = "Option|Shift"; chars = "\\x1b>"; } # Alt + >
-          { key = "Comma"; mods = "Option|Shift"; chars = "\\x1b<"; } # Alt + <
-          { key = "Minus"; mods = "Option|Shift"; chars = "\\x1b_"; } # Alt + _
-          { key = "Key5"; mods = "Option|Shift"; chars = "\\x1b%"; } # Alt + %
-          { key = "Key6"; mods = "Option|Shift"; chars = "\\x1b^"; } # Alt + ^
-          { key = "Backslash"; mods = "Option"; chars = "\\x1b\\\\"; } # Alt + \
-          { key = "Backslash"; mods = "Option|Shift"; chars = "\\x1b|"; } # Alt + |
-        ];
-        env.TERM = "xterm-256color";
-        alt_send_esc = true;
-        scrolling.multiplier = 1;
-        font.normal.family = "JetBrainsMono Nerd Font";
-        font.size = 12;
-        colors = {
-          background = "#282828";
-          foreground = "#EBDBB2";
-          normal = {
-            black = "0x282828";
-            red = "0xcc241d";
-            green = "0x98971a";
-            yellow = "0xd79921";
-            blue = "0x458588";
-            magenta = "0xb16286";
-            cyan = "0x689d6a";
-            white = "0xa89984";
-          };
 
-          bright = {
-            black = "0x928374";
-            red = "0xfb4934";
-            green = "0xb8bb26";
-            yellow = "0xfabd2f";
-            blue = "0x83a598";
-            magenta = "0xd3869b";
-            cyan = "0x8ec07c";
-            white = "0xebdbb2";
-          };
-        };
-      };
+    programs.tmux = {
+      enable = true;
+      keyMode = "vi";
+      history = 10000;
+      plugins = with pkgs.tmuxPlugins [
+        vim-tmux-navigator
+        cpu
+      ];
+        extraConfig = ''
+        set -g status-right '#{ram_bg_color} RAM: #{ram_icon} #{ram_percentage} #{cpu_bg_color} CPU: #{cpu_icon} #{cpu_percentage} | %F %H:%M:%S'
+        ## COLORSCHEME: gruvbox dark (medium)
+        set-option -g status "on"
+        
+        # default statusbar color
+        set-option -g status-style bg=colour237,fg=colour223 # bg=bg1, fg=fg1
+        
+        # default window title colors
+        set-window-option -g window-status-style bg=colour214,fg=colour237 # bg=yellow, fg=bg1
+        
+        # default window with an activity alert
+        set-window-option -g window-status-activity-style bg=colour237,fg=colour248 # bg=bg1, fg=fg3
+        
+        # active window title colors
+        set-window-option -g window-status-current-style bg=red,fg=colour237 # fg=bg1
+        
+        # pane border
+        set-option -g pane-active-border-style fg=colour250 #fg2
+        set-option -g pane-border-style fg=colour237 #bg1
+        
+        # message infos
+        set-option -g message-style bg=colour239,fg=colour223 # bg=bg2, fg=fg1
+        
+        # writing commands inactive
+        set-option -g message-command-style bg=colour239,fg=colour223 # bg=fg3, fg=bg1
+        
+        # pane number display
+        set-option -g display-panes-active-colour colour250 #fg2
+        set-option -g display-panes-colour colour237 #bg1
+        
+        # clock
+        set-window-option -g clock-mode-colour colour109 #blue
+        
+        # bell
+        set-window-option -g window-status-bell-style bg=colour167,fg=colour235 # bg=red, fg=bg
+        
+        ## Theme settings mixed with colors (unfortunately, but there is no cleaner way)
+        set-option -g status-justify "left"
+        set-option -g status-left-style none
+        set-option -g status-left-length "80"
+        set-option -g status-right-style none
+        set-option -g status-right-length "80"
+        set-window-option -g window-status-separator ""
+        
+        set-option -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]"
+        set-option -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d  %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] #h "
+        
+        set-window-option -g window-status-current-format "#[bg=colour214,fg=colour237,nobold,noitalics,nounderscore]#[bg=colour214,fg=colour239] #I #[bg=colour214,fg=colour239,bold] #W#{?window_zoomed_flag,*Z,} #[bg=colour237,fg=colour214,nobold,noitalics,nounderscore]"
+        set-window-option -g window-status-format "#[bg=colour239,fg=colour237,noitalics]#[bg=colour239,fg=colour223] #I #[bg=colour239,fg=colour223] #W #[bg=colour237,fg=colour239,noitalics]"
+      '';
     };
+
+    #    programs.kitty = {
+    #      enable = true;
+    #      theme = "Gruvbox Dark Hard";
+    #      settings = {
+    #        font_family = "JetBrainsMono NF";
+    #        font_size = "12.0";
+    #      };
+    #
+    #    };
 
     home.activation = {
       aliasApplications =
