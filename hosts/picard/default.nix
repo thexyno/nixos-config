16bd7cfced7980aa199db7387fa9ebe1d5ff16a8
Philipp Hochkamp <git@phochkamp.de>
2022-02-22 22:41:43 +0100

    fix

--- hosts/picard/default.nix
+++ hosts/picard/default.nix
@@ -14,6 +14,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
+  boot.loader.systemd-boot.enable = false;
 
 
   networking.interfaces."ens3" = {
