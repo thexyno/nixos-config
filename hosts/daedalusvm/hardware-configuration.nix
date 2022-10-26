f3c11facc8400409b4eed96cad53269755f77e1e
Philipp Hochkamp <git@phochkamp.de>
2022-10-26 21:01:10 +0200

    new vm

--- hosts/daedalusvm/hardware-configuration.nix
+++ hosts/daedalusvm/hardware-configuration.nix
@@ -4,22 +4,36 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ "${modulesPath}/profiles/qemu-guest.nix" ];
-  boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "usbhid" "usb_storage" "sr_mod" "virtiofs" ];
+  imports =
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  networking.interfaces.enp0s1.useDHCP = true;
-  swapDevices = [{ device = "/dev/disk/by-label/NIXOS_SWAP"; }];
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/NIXOS_BOOT";
-    fsType = "vfat";
-  };
+  boot.initrd.availableKernelModules = [ "virtio_pci" "xhci_pci" "usb_storage" "usbhid" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/14a6e16d-ee41-4ad9-bd97-e7a96ca9fa61";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/1695-4C0D";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/c7a01b09-8ae7-43c1-b09c-550df57eb3a1"; }];
   fileSystems."/tmp/rosetta" = {
     device = "rosetta";
     fsType = "virtiofs";
   };
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s1.useDHCP = lib.mkDefault true;
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS_ROOT";
-    fsType = "ext4";
-  };
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
