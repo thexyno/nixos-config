c2a8bd6db12662396166721f648ef55e740cc3b5
Philipp Hochkamp <git@phochkamp.de>
2023-01-14 22:37:17 +0100

    tmux split stuff

--- hm-imports/tmux/default.nix
+++ hm-imports/tmux/default.nix
@@ -1,16 +1,20 @@
-{ config, pkgs,...}:{
-    programs.tmux = {
-      enable = true;
-      keyMode = "vi";
-      clock24 = true;
-      historyLimit = 10000;
-      plugins = with pkgs.tmuxPlugins; [
-        vim-tmux-navigator
-        gruvbox
-      ];
-      extraConfig = ''
-        new-session -s main
-        bind-key -n C-a send-prefix
-      '';
-    };
+{ config, pkgs, ... }: {
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    clock24 = true;
+    historyLimit = 10000;
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
+      gruvbox
+    ];
+    extraConfig = ''
+      new-session -s main
+      bind-key -n C-e send-prefix
+      bind '"' split-window -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+
+    '';
+  };
 }
